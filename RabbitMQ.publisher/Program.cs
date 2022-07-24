bce592d66be5af722c97fea17e20fb4c603d19ad
Muhammed Furkan Baltacı <fbaltaci.34@gmail.com>
2022-07-24 19:55:15 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://gbcsgwch:bA7BS6rPwZZBhem5qzkWwBL-MWmGem6c@chimpanzee.rmq.cloudamqp.com/gbcsgwch");
+
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            //kuyruğu publisher'a oluşturmak istemiyoruz. 'Fanout Exchange'
+           // channel.QueueDeclare("hello", true, false, false);
+        }
+    }
+}
